fce6e2b1338430b6bb14390ad6b2e9b309212c10
Your Name <you@example.com>
2023-03-18 20:15:49 +0300

    new challenge

---
+++ Solutions/str_len_comparison.js
@@ -0,0 +1,25 @@
+function str_len_comparison(words) {
+    // write your code here
+    if(words.length == 0 || words.length == 1){
+        return false;
+    }
+
+    let len = null;
+
+    for (let index = 0; index < words.length; index++) {
+        const element = words[index];
+        if(len == null){
+            len = element.length;
+        }else if (len !== element.length){
+            len = null;
+        }else {
+            len = element;
+        }
+        console.log(len)
+    }
+
+    return len ? true : false;
+
+}
+
+console.log(str_len_comparison(["111", '222', '333']))
